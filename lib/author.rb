e49698666e3bdfd3f53e2610b91a2736b34f519c
J Aaron Brooks Roberts <abroberts5@gmail.comabroberts5@gmail.comlsssh-keygen -t rsa -C abroberts5@gmail.com>
2018-09-26 11:46:45 -0600

    update to author test

--- lib/author.rb
+++ lib/author.rb
@@ -1,3 +1,14 @@
 class Author
+  attr_reader     :books,
+                  :author_info
+
+  def initialize(author_hash)
+    @author_info  = author_hash
+    @books        = []
+  end
+
+  def add_book(title, publication_date)
+    title
+  end
 
 end

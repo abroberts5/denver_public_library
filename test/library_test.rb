89479925566c511471206929841dcbc6fe0f81cc
J Aaron Brooks Roberts <abroberts5@gmail.comabroberts5@gmail.comlsssh-keygen -t rsa -C abroberts5@gmail.com>
2018-09-26 13:08:37 -0600

    library class

--- test/library_test.rb
+++ test/library_test.rb
@@ -1,6 +1,13 @@
 require './test/test_helper'
 
 class LibraryTest < Minitest::Test
+  def test_it_exists
+    dpl = Library.new
+    assert_instance_of Library, dpl
+  end
 
-
+  def test_books_method_starts_empty
+    dpl = Library.new
+    assert_equal [], dpl.books
+  end
 end

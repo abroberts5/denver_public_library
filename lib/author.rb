f49023f3cbdee7cb6369d8c03d824e7ad34581c3
J Aaron Brooks Roberts <abroberts5@gmail.comabroberts5@gmail.comlsssh-keygen -t rsa -C abroberts5@gmail.com>
2018-09-26 12:33:50 -0600

    update to add_book method

--- lib/author.rb
+++ lib/author.rb
@@ -1,14 +1,22 @@
+require_relative './book'
+
 class Author
   attr_reader     :books,
                   :author_info
 
   def initialize(author_hash)
+    @first_name   = author_hash[:first_name]
+    @last_name    = author_hash[:last_name]
     @author_info  = author_hash
     @books        = []
   end
 
   def add_book(title, publication_date)
-    title
+    new_book = Book.new({author_first_name: @first_name,
+      author_last_name: @last_name, publication_date: publication_date,
+      title: title})
+    @books << new_book
+    new_book
   end
 
 end
